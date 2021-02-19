10baf9cf395fd80fbcf8eccf9b9d76c502db2670
Carla Holcomb <carla.programmer@gmail.com>
2021-02-19 04:38:53 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,11 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      const band = { id: Math.random(), name: action.name }
+    
       return { ...state, bands: [...state.bands, action.name] }
 
       case 'DELETE_BAND':
